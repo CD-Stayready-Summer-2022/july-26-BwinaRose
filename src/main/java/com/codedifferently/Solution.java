2dce10c05d01ef0654a50fe3e678520edbdbc469
BwinaRose <sabrinakrueger24@gmail.com>
2022-07-26 09:08:54 -0400

    done

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -1,7 +1,20 @@
 package com.codedifferently;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public String howManyLettersDoYouSee(String input){
-        return null;
+        Map<Character, Integer> chars = new HashMap<>();
+        StringBuilder builder = new StringBuilder();
+        char[] charsArr = input.toCharArray();
+        for (char c : charsArr) {
+            chars.merge(c, 1, Integer::sum);
+        }
+        for (Map.Entry entry : chars.entrySet()) {
+            builder.append(entry.getKey() + ":" + entry.getValue()+ " ");
+        }
+        builder.deleteCharAt(builder.length()-1);
+        return builder.toString();
     }
 }
